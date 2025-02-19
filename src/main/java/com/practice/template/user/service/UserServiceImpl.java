5a060993d6e182fda1e59cdb4dd5c1b4171d9d0f
이현승 <lhseunge@gmail.com>
2025-02-19 20:32:17 +0900

    UserService.getUserById() 구현

--- src/main/java/com/practice/template/user/service/UserServiceImpl.java
+++ src/main/java/com/practice/template/user/service/UserServiceImpl.java
@@ -1,7 +1,10 @@
 package com.practice.template.user.service;
 
+import com.practice.template.user.entity.QUser;
+import com.practice.template.user.entity.User;
 import com.practice.template.user.repository.UserRepository;
 import com.practice.template.user.dto.UserResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +15,26 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final JPAQueryFactory queryFactory;
 
     @Override
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::from)
                 .toList();
+    }
+
+    @Override
+    public UserResponse getUserById(Long id) {
+
+        QUser qUser = QUser.user;
+        UserResponse.from(
+                (User) queryFactory.select()
+                        .from(qUser)
+                        .where(qUser.id.eq(id))
+                        .fetchOne());
+
+
+        return null;
     }
 }
