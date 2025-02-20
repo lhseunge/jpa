dea9a4d12d3c817294543a62dd245b9d3bf36362
lhseunge <lhseunge@gmail.com>
2025-02-20 18:35:27 +0900

    UserServiceImpl.getUserById 로직변경 1. queryFactory.select().from() -> selectFrom() 2. null 예외처리 추가

--- src/main/java/com/practice/template/user/service/UserServiceImpl.java
+++ src/main/java/com/practice/template/user/service/UserServiceImpl.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static com.practice.template.user.entity.QUser.user;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -27,12 +29,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponse getUserById(Long id) {
 
-        QUser qUser = QUser.user;
-        return UserResponse.from(
-                queryFactory.select(qUser)
-                        .from(qUser)
-                        .where(qUser.id.eq(id))
-                        .fetchOne());
+        // select절 Entity와 from절 Entity가 동일할 경우 축약 가능
+        // select(Entity).from(Entity) -> selectFrom(Entity)
+        User userInfo = queryFactory.selectFrom(user)
+                .where(user.id.eq(id))
+                .fetchOne();
+
+        if (userInfo == null) {
+            throw new RuntimeException("user not found");
+        }
+
+        return UserResponse.from(userInfo);
 
 
     }
