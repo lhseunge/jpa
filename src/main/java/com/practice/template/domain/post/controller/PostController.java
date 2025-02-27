521722d836f55a764d81349770b03d8d88ed7c02
lhseunge <lhseunge@gmail.com>
2025-02-27 21:40:34 +0900

    PostController.getPostsByUserId API 추가

--- src/main/java/com/practice/template/domain/post/controller/PostController.java
+++ src/main/java/com/practice/template/domain/post/controller/PostController.java
@@ -1,11 +1,24 @@
 package com.practice.template.domain.post.controller;
 
+import com.practice.template.domain.post.dto.PostResponse;
+import com.practice.template.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("posts")
 @RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
+    @GetMapping("users/{userId}")
+    public List<PostResponse> getPostsByUserId(@PathVariable("userId") Long userId) {
+        return postService.getPostByUserId(userId);
+    }
 }
