449b2a0d74ed11e143c18cb4f688c07c42767b2f
lhseunge <lhseunge@gmail.com>
2025-02-27 21:39:29 +0900

    Post Api prefix 변경

--- src/main/java/com/practice/template/domain/post/controller/PostController.java
+++ src/main/java/com/practice/template/domain/post/controller/PostController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("post")
+@RequestMapping("posts")
+@RequiredArgsConstructor
 public class PostController {
 
 }
