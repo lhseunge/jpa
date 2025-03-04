f6871a0b345e8e927fa43e64eb6b3429fae242d7
lhseunge <lhseunge@gmail.com>
2025-03-04 20:23:14 +0900

    EntityManager @PersistenceContext 적용

--- src/main/java/com/practice/template/core/config/config.java
+++ src/main/java/com/practice/template/core/config/config.java
@@ -2,15 +2,15 @@ package com.practice.template.core.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class config {
 
-    private final EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory getQueryFactory() {
