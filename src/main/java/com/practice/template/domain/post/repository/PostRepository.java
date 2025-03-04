26988aec3addd81c5183bd8bb3cd90efcf894138
lhseunge <lhseunge@gmail.com>
2025-03-04 20:24:08 +0900

    PostRepository.findByUserId 메소드 삭제

--- src/main/java/com/practice/template/domain/post/repository/PostRepository.java
+++ src/main/java/com/practice/template/domain/post/repository/PostRepository.java
@@ -4,10 +4,7 @@ import com.practice.template.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository {
 
-    List<Post> findByUserId(long user_id);
 }
